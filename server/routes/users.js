d6589e0db845ca428a21953dba756e9a5f3a2b1c
WepApp07 <AmineZouguig@proton.me>
2024-02-25 05:09:07 -0800

    Jwt user Verification (Authorization)

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,35 @@
 import express from "express";
-import { test } from "../controllers/user.js";
+import {
+  update,
+  deleteUser,
+  getUser,
+  subscribe,
+  unsubscribe,
+  like,
+  dislike,
+} from "../controllers/user.js";
 
 const router = express.Router();
+
+// update user
+router.put("/:id", update);
+
+// delete user
+router.delete("/:id", deleteUser);
+
+// get a user
+router.get("/find/:id", getUser);
+
+// subscribe a user
+router.put("/sub/:id", subscribe);
+
+// unsubscribe
+router.put("/unsub/:id", unsubscribe);
+
+// like a video
+router.put("/like/:videoId", like);
+
+// get a user
+router.get("/dislike/:videoId", dislike);
 
 export default router;
