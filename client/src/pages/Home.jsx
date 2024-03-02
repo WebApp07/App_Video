9570c9eb64a5f3244826ff4a9236989c3dd4ee35
WepApp07 <AmineZouguig@proton.me>
2024-03-02 12:21:37 +0100

    React Node.js MongoDB Auth (Login/Register)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Card from "../components/Card";
 import axios from "axios";
+
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
@@ -10,15 +11,14 @@ const Container = styled.div`
 
 const Home = ({ type }) => {
   const [videos, setVideos] = useState([]);
-
   useEffect(() => {
     const fetchVideos = async () => {
       const res = await axios.get(`/videos/${type}`);
       setVideos(res.data);
     };
+
     fetchVideos();
   }, [type]);
-
   return (
     <Container>
       {videos.map((video) => (
